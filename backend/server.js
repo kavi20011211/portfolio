96920616ba6fb143ee5da8c2295c8210688d0c2e
kavi20011211 <@Lk6985iop>
2025-02-11 13:22:45 +0530

    Backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
