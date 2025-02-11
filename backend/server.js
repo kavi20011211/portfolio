b28ea64624c5997c8af5b9c1a3a89da185fb292b
kavi20011211 <@Lk6985iop>
2025-02-11 18:48:09 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -4,34 +4,10 @@ const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
 const app = express();
-const corsOptions = {
-    origin: (origin, callback) => {
-        const allowedOrigins = ['https://portfolio-me-pink-one.vercel.app'];
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
-};
-app.use(cors(corsOptions));
+app.use(cors());
 // app.options("*",cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://portfolio-me-pink-one.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Origin, X-Requested-With, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(204);
-    }
-    next();
-});
 
 app.use('/api/users', require('./routes/UserRoutes'));
 
