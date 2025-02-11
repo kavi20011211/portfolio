6efb40d9eaa063cb9f44895c218383d03f24b28b
kavi20011211 <@Lk6985iop>
2025-02-11 20:06:17 +0530

    Sidebat update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <div className="absolute left-5 top-1/2 transform -translate-y-1/2 flex flex-row gap-5 md:flex-col">
+    <div className="absolute left-5 top-1/2 transform -translate-y-1/2 flex flex-row gap-5 mt-20 md:flex-col md:mt-0">
       <a href="https://www.instagram.com/hey_im_kavi.01/" className="text-white text-xl hover:text-orange-500">
-      <i class="bi bi-instagram"></i>
+      <i className="bi bi-instagram"></i>
       </a>
       <a href="https://www.linkedin.com/in/uvindu-suraweera-03b527216/" className="text-white text-xl hover:text-orange-500">
-      <i class="bi bi-linkedin"></i>
+      <i className="bi bi-linkedin"></i>
       </a>
       <a href="https://github.com/kavi20011211" className="text-white text-xl hover:text-orange-500">
-      <i class="bi bi-github"></i>
+      <i className="bi bi-github"></i>
       </a>
     </div>
   );
