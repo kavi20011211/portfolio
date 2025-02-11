a8cdf3912ab3b89199a399bfe2a9f332d03c25fd
kavi20011211 <@Lk6985iop>
2025-02-11 11:38:52 +0530

    Server changes in backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use('/api/users',require('./routes/UserRoutes'));
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(PORT, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
