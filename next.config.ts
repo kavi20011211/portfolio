a5ed6bd6c8897a5663327f7375ac6cb5858f1c7d
kavi20011211 <kavishkauvindu0@gmail.com>
2025-08-02 19:44:26 +0530

    update configs

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false,
-  ignoreDuringBuilds: true,
+  devIndicators: {
+    buildActivity: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
