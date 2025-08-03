092d53eabcc7e83155792e49df1d81e14ee72753
kavi20011211 <kavishkauvindu0@gmail.com>
2025-08-03 22:11:48 +0530

    Head section updated

--- src/app/components/head_section.tsx
+++ src/app/components/head_section.tsx
@@ -21,7 +21,9 @@ export default function HeaderSection() {
 
       <div className="relative z-10 flex items-center justify-center text-center h-full text-white px-5">
         <div className="gap-5 flex flex-col">
-          <h1 className="text-7xl font-black">Code. Create. Disrupt.</h1>
+          <h1 className="text-4xl md:text-7xl font-black">
+            Code. Create. Disrupt.
+          </h1>
           <h4 className="text-2xl md:text-5xl font-semibold">
             Engineering Bold Ideas into Scalable Software
           </h4>
